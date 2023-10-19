23cf85c9d89b9b16c8014809ccd494fc7851df6b
wfceza <wfceza@gmail.com>
2023-10-18 23:47:22 -0700

    made some shanfges

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar () {
+    return (
+        <nav>
+            <img src="images/holding-phone" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+    
+}
