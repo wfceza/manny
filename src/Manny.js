a58b0c02039d8b11225958457070458a6b6268e3
wfceza <wfceza@gmail.com>
2024-07-23 18:12:13 -0700

    s

--- src/Manny.js
+++ src/Manny.js
@@ -1,7 +1,19 @@
+
 export default function Manny () {
+
+    const [isCorrect, setIsCorrect] = React.useState(false)
+
+    const yesAnswer = () => {
+        setIsCorrect(prev => !prev)
+    }
+
+   
     return (
         <div>
             <h1>I AM SO SORRY FOR EVERYTHING MANNY</h1>
+            <h3>Will you forgive me?</h3>
+            <button onClick={yesAnswer}>yes</button>
+            <button>No</button>
         </div>
     )
 }
