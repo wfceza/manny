342fcc32ac210b54e66dd4aed9971d0822f8c80d
wfceza <wfceza@gmail.com>
2024-07-23 19:14:33 -0700

    first commit

--- src/Manny.js
+++ src/Manny.js
@@ -1,4 +1,5 @@
 import React from "react"
+import './App.css'
 export default function Manny () {
 
     const [isCorrect, setIsCorrect] = React.useState(false)
@@ -9,11 +10,13 @@ export default function Manny () {
 
    
     return (
-        <div>
-            <h1>I AM SO SORRY FOR EVERYTHING MANNY</h1>
-            <h3>Will you forgive me?</h3>
+        <div className="heading" >
+            <h1>KAYNE</h1>
+            <img className="image" src="./IMG_2844.JPG" height="100px"/>
+            <h2>I AM SO SORRY FOR EVERYTHING MANNY</h2>
+            <h3>Will you forgive me? &#129402;</h3>
             <button onClick={yesAnswer}>yes</button>
-            {isCorrect && <p>Thank you very much, and i promise not to hurt you agaim</p>}
+            {isCorrect && <p>Thank you very much, I love you and i promise never to hurt you agaim</p>}
         </div>
     )
 }
